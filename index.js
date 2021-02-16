f0318a1781b8acabcc923ddf7476839ce7859f6b
Shekhar Tyagi <notshekhar@gmail.com>
2021-02-16 13:12:21 +0530

    new

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ require("dotenv").config()
 const peerserver = PeerServer({
     port: process.env.PORT || 3001,
     path: "/",
+    config: {
+        iceServers: [
+            { url: "stun:stun.l.google.com:19302" },
+            { url: "stun:stun1.l.google.com:19302" },
+            { url: "stun:stun2.l.google.com:19302" },
+            { url: "stun:stun3.l.google.com:19302" },
+            { url: "stun:stun4.l.google.com:19302" },
+            {
+                url: "turn:192.158.29.39:3478?transport=udp",
+                credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+                username: "28224511:1379330808",
+            },
+            {
+                url: "turn:192.158.29.39:3478?transport=tcp",
+                credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+                username: "28224511:1379330808",
+            },
+        ],
+    },
 })
 peerserver.on("connection", (client) => {
     console.log(`Connection ${client.id}`)
